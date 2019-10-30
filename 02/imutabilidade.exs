bb9c82fea9b34f32769a79f003d3f6cf37b0b7b5
Ricardo Oliveira <rcoproc@gmail.com>
2019-10-30 01:06:34 -0300

    First Commit

---
+++ 02/imutabilidade.exs
@@ -0,0 +1,16 @@
+total = 876
+
+defmodule Mutante do
+  def mutar(valor) do
+    valor = 1
+    IO.puts "interno - #{valor}" # Aqui será exibido 1 ou 876 ?
+    valor
+  end
+end
+
+
+Mutante.mutar(total)
+IO.puts "Externo A- #{total}" # E aqui ? 1 ou 876 ?
+
+total = Mutante.mutar(total) #rebinding
+IO.puts "externo B - #{total}" # E agora, 1 ou 876?
---
+++ 19_structs/user.ex
@@ -0,0 +1,7 @@
+defmodule User do
+  defstruct nome: "Ricardo", age: 47
+end
+
+defmodule UserEmpty do
+  defstruct nome: nil, age: nil
+end
